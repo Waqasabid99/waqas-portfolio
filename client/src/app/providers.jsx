236322ce74c34b3ffb34b837acdaa805b6dff5bb
wa <wa@agiteks.com>
2026-06-27 21:52:08 +0500

    refactor: clean up formatting and add debug log for reCAPTCHA site key in providers

--- client/src/app/providers.jsx
+++ client/src/app/providers.jsx
@@ -1,17 +1,11 @@
 "use client";
-
-import {
-    GoogleReCaptchaProvider
-} from "react-google-recaptcha-v3";
-
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 export default function Providers({ children }) {
-
+    console.log(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY);
     return (
         <GoogleReCaptchaProvider
-            reCAPTCHAKey={
-                process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY
-            }
+            reCAPTCHAKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
         >
             {children}
         </GoogleReCaptchaProvider>
